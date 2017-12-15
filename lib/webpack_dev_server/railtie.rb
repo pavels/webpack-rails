115f144a860c3978a8cb6c5e5732c4f8b1c11e17
Pavel Sorejs <sorejs@gmail.com>
2017-12-15 19:14:11 +0100

    rails 5 support

--- lib/webpack_dev_server/railtie.rb
+++ lib/webpack_dev_server/railtie.rb
@@ -4,7 +4,7 @@ module WebpackDevServer
     initializer :webpack_dev_server, after: :load_config_initializers do |app|
       if ( ::Rails.configuration.webpack.dev_server.enabled && !::Rails.configuration.webpack.dev_server.remote )
         default_proxy = WebpackDevServer.curb_available ? 'Curb' : 'NetHttp'
-        proxy ||= "WebpackDevServer::Middleware::#{default_proxy}Proxy"
+        proxy ||= "WebpackDevServer::Middleware::#{default_proxy}Proxy".constantize
         app.config.middleware.insert_before 0, proxy
       end
     end
